8f915005e4ad99297ab27eb1f9800a21a17a2104
harouny <ahmedelharouny@outlook.com>
2015-02-24 08:10:54 +1100

    initial solution

---
+++ Mooka/Mooka.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Mooka.WinPhone.Resources;
+
+namespace Mooka.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
